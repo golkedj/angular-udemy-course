097b7bbd487b0db586b1ab5f0b4a96032ffaaaea
David Golke <golkedj@gmail.com>
2019-06-07 15:19:18 -0600

    Lecture: Assignment 3: Practicing Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,30 @@ import { Component } from '@angular/core';
     h3 {
       color: dodgerblue
     }
+    .whiteText {
+      color: white
+    }
   `]
 })
 export class AppComponent {
   username = '';
+  buttonClickTimes : Array<Date>= [];
+  buttonDetailsText = 'Display Details';
+  showDetails = false;
+
+  toggleShowDetails() {
+    this.showDetails = !this.showDetails;
+    this.buttonDetailsText = this.showDetails ? 'Hide Details' : 'Display Details';
+    this.buttonClickTimes.push(new Date());
+  }
+
+  getColor(buttonClickTime) {
+    // const index = this.buttonClickTimes.indexOf(buttonClickTime);
+    return this.shouldStyle(buttonClickTime) ? 'blue' : 'transparent';
+  }
+
+  shouldStyle(buttonClickTime) {
+    const index = this.buttonClickTimes.indexOf(buttonClickTime);
+    return (index > 3);
+  }
 }
