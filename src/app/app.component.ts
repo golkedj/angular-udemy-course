5e437681f41c2d75d7b403a99cf2d62b2d1ee281
David Golke <golkedj@gmail.com>
2019-06-11 12:20:08 -0600

    Reverted to base project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue
-    }
-    .whiteText {
-      color: white
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = '';
-  buttonClickTimes : Array<Date>= [];
-  buttonDetailsText = 'Display Details';
-  showDetails = false;
-
-  toggleShowDetails() {
-    this.showDetails = !this.showDetails;
-    this.buttonDetailsText = this.showDetails ? 'Hide Details' : 'Display Details';
-    this.buttonClickTimes.push(new Date());
-  }
-
-  getColor(buttonClickTime) {
-    // const index = this.buttonClickTimes.indexOf(buttonClickTime);
-    return this.shouldStyle(buttonClickTime) ? 'blue' : 'transparent';
-  }
-
-  shouldStyle(buttonClickTime) {
-    const index = this.buttonClickTimes.indexOf(buttonClickTime);
-    return (index > 3);
-  }
 }
