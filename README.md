987ac7846605282966b7597147c1cdff6d7188d4
David Golke <golkedj@gmail.com>
2019-06-24 15:45:19 -0600

    Project rename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularUdemyCourse
+# RecipeBook
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-udemy-course!');
+    expect(page.getTitleText()).toEqual('Recipe Book');
   });
 
   afterEach(async () => {
