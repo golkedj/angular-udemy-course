aec997789bdcee051c3fe3acf6062b11c5dc626f
golkedj <golkedj@gmail.com>
2020-02-22 23:34:35 -0700

    Ensure all recipes have ingredients property

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -17,9 +18,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.recipesUrl).subscribe(recipes => {
-      console.log(recipes);
-      this.recipeService.setRecipes(recipes);
-    });
+    this.http
+      .get<Recipe[]>(this.recipesUrl)
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }))
+      .subscribe(recipes => {
+        console.log(recipes);
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
