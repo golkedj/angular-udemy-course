6da0486e6d5b3813fe39868a0c10c598b86c3aa0
golkedj <golkedj@gmail.com>
2020-02-22 23:20:35 -0700

    Store recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  recipesUrl: string = 'https://ng-course-recipe-book-golkedj.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.recipesUrl, recipes).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
