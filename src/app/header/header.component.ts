595da6c2f95d18bba9204e741a82f26a32258923
golkedj <golkedj@gmail.com>
2020-04-16 00:58:14 -0600

    Get auth from app state in store

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -13,20 +16,26 @@ export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
+  ) { }
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(
-      user => {
-        this.isAuthenticated = !!user;
-      }
-    );
+    this.userSub = this.store
+      .select('auth')
+      .pipe(map(authState => authState.user))
+      .subscribe(
+        user => {
+          this.isAuthenticated = !!user;
+        });
   }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
   }
-  
+
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
   }
